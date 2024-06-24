e5dc0d588aa1fc1ebda298bf4994859d60fe51e4
tcarocci <t.23carocci@yahoo.com>
2024-06-24 15:54:54 -0500

    add index.js to API routes folder for integrating user and thought routes

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+// Imports routes
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+// Uses routes
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
